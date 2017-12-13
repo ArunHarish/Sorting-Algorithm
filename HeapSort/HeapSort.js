289f8b28c2ef355edf4e4d2c9e1d7f6d173565d6
ArunHarish <Arunhari97@yahoo.com>
2017-12-13 13:22:19 +1000

    Refactored the code and fixed a bug in minHeap

--- HeapSort/HeapSort.js
+++ HeapSort/HeapSort.js
@@ -3,7 +3,7 @@
 class HeapSort {
     constructor(input) {
         this.input = input;
-        this.end = input.length;
+        this.end = input.length - 1;
     }
     
     swap(a, b) {
@@ -13,7 +13,6 @@ class HeapSort {
         
         // Using xor algorithm to swap values.
         f = (f ^ s) ^ (s ^= f ^ s);
-        
         this.input[a] = f;
         this.input[b] = s;
     }
@@ -54,13 +53,15 @@ class HeapSort {
             
             //Recursive Case
             
-            if((leftChild >= rightChild || (rightChild == null && leftChild != null)) && currentNode < leftChild) {
-                this.swap(indexLeft, index);
+            if(leftChild != null && (leftChild >= rightChild) && leftChild > currentNode) {
+			    this.swap(indexLeft, index);
                 maxHeapify.call(this, this.parentIndex(index));
-            }
-            else if((rightChild > leftChild || (leftChild == null && rightChild != null)) && currentNode < rightChild) {
-                this.swap(indexRight, index);
+            
+			}
+            else if(rightChild != null && (rightChild >= leftChild) && rightChild > currentNode) {
+				this.swap(indexRight, index);
                 maxHeapify.call(this, this.parentIndex(index));
+            
             }
             
             maxHeapify.call(this, indexRight);
@@ -72,6 +73,7 @@ class HeapSort {
         
         
         while(this.end > 0 && maxHeapify.call(this)) {
+            
             this.swap(0, this.end--);
         }
         
@@ -79,39 +81,47 @@ class HeapSort {
         
     }
     get minHeap() {
+        function minHeapify(index) {
+            //Base Case
+            //Who knew undefined == null 
+            if(index > this.end || index === null)
+                return null;
+                
+            var index = index || 0;
+            var indexLeft = this.leftIndex(index);
+            var indexRight = this.rightIndex(index);
+            
+            var leftChild = input[indexLeft] || null;
+            var rightChild = input[indexRight] || null;
+            var currentNode = input[index];
+            
+            //Recursive Case
+            
+            if(leftChild != null && (leftChild <= rightChild)  && currentNode > leftChild) {
+            //if(leftChild != null && (rightChild >= leftChild && currentNode > leftChild)) {    
+			    this.swap(indexLeft, index);
+                minHeapify.call(this, this.parentIndex(index));
+            
+			}
+            else 
+            if(rightChild != null && (rightChild < leftChild)  && currentNode > rightChild) {
+				this.swap(indexRight, index);
+                minHeapify.call(this, this.parentIndex(index));
+            
+            }
+            
+            minHeapify.call(this, indexRight);
+            minHeapify.call(this, indexLeft);
+            
+            
+            return true;
+        }
         
+        
+        while(this.end > 0 && minHeapify.call(this)) {
+            
+            this.swap(0, this.end--);
+        }
+        return this.input;
     }
 }
-
-var input = [
-    28,
-    48,
-    19,
-    71,
-    15,
-    72,
-    82,
-    68,
-    92,
-    40,
-    18,
-    19,
-    30,
-    35,
-    96,
-    28,
-    42,
-    69,
-    98,
-    71,
-    57,
-    88,
-    50,
-    17,
-    38,
-    19,
-    6,
-    1200
-];
-var a = new HeapSort(input);
-console.log(a.maxHeap)
