34a3a440f54531298371dba66d92cd98baf865a1
Harish <Arunhari97@yahoo.com>
2017-12-15 19:46:17 +1000

    Added an update

--- MergeSort/MergeSort.js
+++ MergeSort/MergeSort.js
@@ -4,6 +4,30 @@ class MergeSort {
     constructor() {
         
     }
+
+    comparator() {
+        if(this.sortType == -1)
+            return ;
+        return ;
+    }
+
+    mergeSort() {
+        console.log(true);
+        //this.mergeSort();
+    }
+
+    sort(a) {
+        if(a < 0) {
+            this.sortType = -1;
+            this.mergeSort();
+            return ;
+        }
+        
+        this.sortType = 1;
+        this.mergeSort();
+        return ;
+    }
+
 }
 
 module.exports = MergeSort;
--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var MergeSort = require("./MergeSort/MergeSort.js");
 var HeapSort = require("./HeapSort/HeapSort.js");
 
 function generateRandom() {
-    var random = new Array(10);
+    var random = new Array(1000);
     for(var x = 0; x < random.length; x++) {
-        random[x] = Math.floor(1 + Math.random() * 1000);
+        random[x] = Math.floor(1 + Math.random() * 10000);
     }
     return random;
 }
@@ -14,20 +14,18 @@ function generateRandom() {
 function test() {
     var t1 = (new Date()).getTime();
     var t2;
-    var heapSample = [10, 28, 128, 89, 257, 492, 587, 856, 243, 861];
-    var quickSample =[10, 28, 128, 89, 257, 492, 587, 856, 243, 861];
-        
+    var heapSample = generateRandom();
+    var quickSample = JSON.parse(JSON.stringify(heapSample));
     //Function goes here
-    var hs = new HeapSort(heapSample);
+   
+
+    t1 = (new Date().getTime());
     var qs = new QuickSort(quickSample);
-    hs.sort(-1);
+    console.log("Started Quick sort...");
     qs.sort(-1);
-    console.log(heapSample, quickSample);
-    
-    //Constant
     t2 = (new Date()).getTime();
-    console.log("______________________");
-    console.log(t2 - t1);
+    console.log("Quick Sort Time: "+ (t2 - t1));
+
 }
 
 test();
