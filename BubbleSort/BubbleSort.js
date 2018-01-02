c4a2e51ac8c4d9c9a78c15e708614254ac5a348e
Arun97 <Arunhari97@yahoo.com>
2018-01-02 16:44:03 +1000

    Included BubbleSort content

--- BubbleSort/BubbleSort.js
+++ BubbleSort/BubbleSort.js
@@ -1,8 +1,48 @@
 "use strict";
 class BubbleSort {
-    constructor() {
-        
+    constructor(input) {
+        this.input = input;
+        this.sortType = 1;
     }
+
+    swap(a, b) {
+        let temp = this.input[a];
+        this.input[a] = this.input[b];
+        this.input[b] = temp;
+    }
+
+    comparator(a, b) {
+        if(this.sortType == 1)
+            return b > a;
+        return a > b;
+    }
+
+    bubbleSort() {
+        let reference = this.input;
+        const arrayLength = reference.length;
+
+        for(let x = arrayLength; x > 0; x--) {
+            for(let y = 1; y < x; y++) {
+                if(this.comparator(reference[y], reference[y - 1])) {
+                    this.swap(y, y - 1);
+                }
+            }
+        }
+    }
+
+    sort(order) {
+        if(order < 0) {
+            this.sortType = -1;
+            this.bubbleSort();
+            return ;
+        }
+
+        this.sortType = 1;
+        this.bubbleSort();
+        return ;
+
+    }
+
 }
 
 module.exports = BubbleSort;
